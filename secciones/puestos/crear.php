8452b72e38138bbd802accbe476bb4a5d1635c99
Lalord22 <lalo22296@hotmail.com>
2023-07-19 19:33:13 -0600

    index create usuario y puesto

--- secciones/puestos/crear.php
+++ secciones/puestos/crear.php
@@ -1,5 +1,31 @@
 <?php include("../../templates/header.php"); ?>
 
-Crear puestos
+<br/>
+
+<div class="card">
+    <div class="card-header">
+        Puestos
+    </div>
+    <div class="card-body">
+        
+        <form action="" method="post" enctype="multipart/form-data">
+
+            <div class="mb-3">
+              <label for="nombredelpuesto" class="form-label">Nombre del puesto</label>
+              <input type="text"
+                class="form-control" name="nombredelpuesto" id="nombredelpuesto" aria-describedby="helpId" placeholder="Nombre del puesto">
+              
+            </div>
+
+            <button type="submit" class="btn btn-success">Agregar</button>
+            <a name="" id="" class="btn btn-danger" href="index.php" role="button">Cancelar</a>
+
+        </form>
+
+    </div>
+    
+</div>
+
+
 
 <?php include("../../templates/footer.php"); ?>
